71c04fb3569ddc69864128bbaa31babc815aba4b
Tareq69 <mtareq880@gmail.com>
2021-06-02 19:25:42 +0600

    First commit of learningpython

---
+++ hello.py
@@ -0,0 +1,19 @@
+print("Hello World")
+print("Why am i like this?")
+# x = 5
+y = 10
+
+# result = x + y # adding two numbers
+#print("Result is: ", result)
+#print(type(result))
+#a,b,c = "apple","Orange","Banana"
+#print(c)
+
+
+
+print(10>9)
+print(bool("Hello"))
+print(bool(0))
+print(5**2)
+txt = input ("Enter your desired quote")
+print("The best answer is to act",txt)
